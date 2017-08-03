241fe4c23399cae96f0dca7e761611398b602ea1
depifanio.jerod <depifanio.jerod@mahindra.com>
2017-08-03 12:44:23 -0400

    access test

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+header( 'Location: /index.html' ) ;
+//echo '<script type="text/javascript", src="../lib/game.js">';
+
+?>
--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-//header( 'Location: /index.html' ) ;
-echo '<script type="text/javascript", src="../lib/game.js">';
-
-?>
