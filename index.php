befe6a551f84d947b47aa0a9eb1eb96b4d0567af
Askbackwards <jjbaddude@hotmail.com>
2017-08-04 10:49:26 -0400

    html fix

--- index.php
+++ index.php
@@ -27,11 +27,10 @@ try {
     file_put_contents($game, $gameText['Body']);
 
     // Run game script
-    echo "
-<!DOCTYPE html>
+    echo "<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
+    <meta charset='utf-8'>
     <title>PlayCanvas Hello Cube</title>
     <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no' />
     <style>
