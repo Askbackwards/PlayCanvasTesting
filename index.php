935b8587f1b832c8d24a610fabccda7dcdbb1a86
Askbackwards <jjbaddude@hotmail.com>
2017-08-04 09:51:33 -0400

    Working maybe?

--- index.php
+++ index.php
@@ -18,14 +18,13 @@ $s3 = new Aws\S3\S3Client([
 
 try {
     // Get the object
-    $result = $s3->getObject(array(
+    $game = $s3->getObject(array(
         'Bucket' => $bucket,
         'Key'    => $keyname
     ));
 
-    // Display the object in the browser
-    header("Content-Type: {$result['ContentType']}");
-    echo $result['Body'];
+    // Run game script
+    echo '<script type="text/javascript" src=', $game, '></script>';
 } catch (S3Exception $e) {
     echo $e->getMessage() . "\n";
 }
