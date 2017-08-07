8e47ad74818f2d7c9a260c7a4a8f6a5d2ced60ca
Askbackwards <jerod.dep@gmail.com>
2017-08-07 10:56:14 -0400

    Server update

--- web/index.php
+++ web/index.php
@@ -1,39 +1,3 @@
 <?php
 header( 'Location: /index.html' ) ;
-
-/*require 'aws.phar';
-
-use Aws\S3\S3Client;
-use Aws\S3\Exception\S3Exception;
-
-$bucket = getenv('S3_BUCKET_NAME');
-$keyname = 'game.js';
-
-//Instantiate the client
-$s3 = new Aws\S3\S3Client([
-    'version' => 'latest',
-    'region'  => 'us-east-2'
-]);
-
-
-try {
-    // Get the object
-    $gameText = $s3->getObject(array(
-        'Bucket' => $bucket,
-        'Key'    => $keyname,
-    ));
-    
-    $game="game.js";
-    file_put_contents($game, $gameText['Body']);
-    
-    chmod($game, 0600);
-*/
-
-    // Run game script
-/*} catch (S3Exception $e) {
-    echo $e->getMessage() . "\n";
-}*/
-
-
-
 ?>
--- web/scripts/server.js
+++ web/scripts/server.js
@@ -1,9 +1,32 @@
-var server = require ('http').createServer();
+var server = require('http').createServer();
 var io = require('socket.io')(server);
 
-io.sockets.on('connection', function(socket) {
+var players = [];
 
+function Player (id) {
+    this.id = id;
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+    this.entity = null;
+}
+
+io.sockets.on('connection', function(socket) {
+    socket.on (‘initialize’, function () {
+        var idNum = players.length;
+        var newPlayer = new Player (idNum);
+        // Creates a new player object with a unique ID number.
+
+        players.push (newPlayer);
+        // Adds the newly created player to the array.
+
+        socket.emit (‘playerData’, {id: idNum, players: players});
+        // Sends the connecting client his unique ID, and data about the other players already connected.
+
+        socket.broadcast.emit (‘playerJoined’, newPlayer);
+        // Sends everyone except the connecting player data about the new player.
+    });
 });
 
-console.log('Server startecd.');
+console.log ('Server started.');
 server.listen(3000);
